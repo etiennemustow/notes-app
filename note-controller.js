b85ff54691e6b3c7aeee108578901bbb47508577
etiennemustow <etienne.o.mustow@gmail.com>
2017-10-11 12:31:22 +0100

    Spiked: note-controller.js can display NoteListView text

--- note-controller.js
+++ note-controller.js
@@ -1,4 +1,17 @@
-function changeGreeting(newgreeting) {
-  var element = document.getElementById("app");
-  element.innerHTML = newgreeting
+(function(exports){
+
+function NoteController(notelist = new Notelist()){
+  this.notelist = notelist
+  var note = this.notelist.store("Favourite drink: seltzer", Note)
+  this.view = new NoteListView()
+  this.view.list = this.notelist
 }
+
+NoteController.prototype.getHTML = function(){
+  var rendered = this.view.render()
+  var element = document.getElementById("note");
+  element.innerHTML = rendered
+}
+
+exports.NoteController = NoteController;
+})(this);
